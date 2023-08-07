d1a298de4a4b7863beee8f2bab5cb34b88630027
Kagisokgomo <kagisomalobela29@gmail.com>
2023-08-07 06:53:37 -0700

    Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -10,7 +10,7 @@ int _atoi(char *s)
 	int sig = 1, i = 0;
 	unsigned int unsig = 0;
 
-	while (!(s[i] <= '9' && a[i] >= '0') && s[i] != '\0')
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sig *= -1;
