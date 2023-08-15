cf5e2ff6f75a5786eec4f7daa19cfa2767870522
Kagisokgomo <kagisomalobela29@gmail.com>
2023-08-15 05:51:28 -0700

    Poppy

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog -the new dog struct whoose variable is to be
+ * initialized
+ *
+ * @d: pointer to the struct of the new dog
+ * @name: name of the new dog
+ * @age: age of the new dog
+ * @owner: the owner of the new dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return (NULL);
+	(*d).name = name;
+	d->age = age;
+	d->owner = owner;
+}
