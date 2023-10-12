e845ec7024cf826730f47367ce7def474b2fdb6d
Kagiso <kagisomalobela29@gmail.com>
2023-10-12 06:48:34 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -35,6 +35,7 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
+
 	return (a * b);
 }
 
@@ -47,12 +48,6 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a / b);
 }
 
