e557a962317294197ce1e28ad3b644213b4fe280
Kagiso <kagisomalobela29@gmail.com>
2023-12-22 06:25:55 -0800

    ['Betty'] = 'Cool'

--- 0x1A-hash_tables/100-main.c
+++ 0x1A-hash_tables/100-main.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	shash_table_t *ht;
-	
+
 	ht = shash_table_create(1024);
 	shash_table_set(ht, "y", "0");
 	shash_table_print(ht);
@@ -30,6 +30,6 @@ int main(void)
 	shash_table_set(ht, "m", "7");
 	shash_table_print(ht);
 	shash_table_print_rev(ht);
-        shash_table_delete(ht);
+	shash_table_delete(ht);
 	return (EXIT_SUCCESS);
 }
